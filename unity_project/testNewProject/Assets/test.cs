025c94877360333dfbeb2510b69867fd830c756f
Chayakron Pengchamsri <chayakron8@gmail.com>
2017-06-09 18:31:59 +0700

    add test game play

--- unity_project/testNewProject/Assets/test.cs
+++ unity_project/testNewProject/Assets/test.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class test : MonoBehaviour {
 
+	public float moveSpeed = -1;
+	public float jumpForce ;
+	public KeyCode startKey;
+
+
+	private Rigidbody rb2;
+	private int checkStart = 0 ;
+
+
+
 	// Use this for initialization
 	void Start () {
-		
+		rb2 = GetComponent<Rigidbody> ();
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown (startKey)) {
+			checkStart = 1;
+		}
+		if (checkStart == 1) {
+			rb2.MovePosition ( new Vector3 (rb2.position.x, rb2.position.y, rb2.position.z)+
+							   new Vector3 (-1, 0, 0) *
+							   Time.deltaTime);
+		}
+
 	}
+
+
+
 }
---
+++ unity_project/testNewProject/Assets/testContol.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testContol : MonoBehaviour {
+
+	public float moveSpeed ;
+	public float jumpForce ;
+	public KeyCode moveUp;
+
+
+	private Rigidbody rb;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (moveUp)) {
+			rb.velocity = new Vector3 (rb.velocity.x, jumpForce ,rb.velocity.z);
+		}
+		
+	}
+}
