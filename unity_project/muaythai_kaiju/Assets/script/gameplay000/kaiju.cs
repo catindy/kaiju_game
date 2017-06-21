0398b6b2b2e0f9226fca0ddaa53ef32a3a0849cb
Chayakron Pengchamsri <chayakron8@gmail.com>
2017-06-21 18:15:42 +0700

    add ระเบิด

--- unity_project/muaythai_kaiju/Assets/script/gameplay000/kaiju.cs
+++ unity_project/muaythai_kaiju/Assets/script/gameplay000/kaiju.cs
@@ -1,14 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class kaiju : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+
+
+
+	void Explode() {
+		var exp = GetComponent<ParticleSystem>();
+		exp.Play();
+
 	}
-	
+
+	void Start() {
+
+	}
+
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.name == "hero") {
+			//Destroy (other.gameObject, 1f);
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+		} else {
+			Destroy(other.gameObject,1f);
+			Explode();
+		}
+	}
 	// Update is called once per frame
 	void Update () {
 		
